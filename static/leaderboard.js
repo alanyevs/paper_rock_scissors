622139134f22546b440b9c8154dfbb04ab203c05
Yanghui Pang <yanghuipang288@gmail.com>
2022-04-26 17:44:51 -0400

    leaderboard api

--- static/leaderboard.js
+++ static/leaderboard.js
@@ -1,30 +1,33 @@
-let data=[
-    {
-        "rank":"1",
-        "user_id":"alanyevs",
-        "winning_rate":"20%",
-        "games":"200"
-    }
-]
-
 let update_time = "Feb 27, 2022 16:30PM (EST)"
 
+function GetRank() {
+    return sdk.leaderboardGet({}, {}, {});
+}
+
 $(document).ready(function(){
-    $("#last_update").html("Last update: "+update_time)
-    $.each(data, function(i, d){
-        let r = $("<div class='row'>")
-        let c = $("<div class='col-1'>")
-        c.html(d.rank)
-        r.append(c)
-        c = $("<div class='col-2'>")
-        c.html(d.user_id)
-        r.append(c)
-        c = $("<div class='col-2'>")
-        c.html(d.winning_rate)
-        r.append(c)
-        c = $("<div class='col-2'>")
-        c.html(d.games)
-        r.append(c)
-        $("#leaderboard_container").append(r)
+    $("#last_update").html("Last update: " + update_time)
+    GetRank()
+    .then((response) => {
+        console.log(response);
+        let data = response.data.body;
+        $.each(data, function(i, d){
+            let r = $("<div class='row'>")
+            let c = $("<div class='col-1'>")
+            c.html(d.Rank)
+            r.append(c)
+            c = $("<div class='col-2'>")
+            c.html(d.UserName)
+            r.append(c)
+            c = $("<div class='col-2'>")
+            c.html(d.Score)
+            r.append(c)
+            c = $("<div class='col-2'>")
+            c.html(d.GameCount)
+            r.append(c)
+            $("#leaderboard_container").append(r)
+        })
     })
+    .catch((error) => {
+        console.log(error);
+    });
 })
---
+++ static/sdk/apigClient.js
@@ -0,0 +1,285 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+var apigClientFactory = {};
+apigClientFactory.newClient = function (config) {
+    var apigClient = { };
+    if(config === undefined) {
+        config = {
+            accessKey: '',
+            secretKey: '',
+            sessionToken: '',
+            region: '',
+            apiKey: undefined,
+            defaultContentType: 'application/json',
+            defaultAcceptType: 'application/json'
+        };
+    }
+    if(config.accessKey === undefined) {
+        config.accessKey = '';
+    }
+    if(config.secretKey === undefined) {
+        config.secretKey = '';
+    }
+    if(config.apiKey === undefined) {
+        config.apiKey = '';
+    }
+    if(config.sessionToken === undefined) {
+        config.sessionToken = '';
+    }
+    if(config.region === undefined) {
+        config.region = 'us-east-1';
+    }
+    //If defaultContentType is not defined then default to application/json
+    if(config.defaultContentType === undefined) {
+        config.defaultContentType = 'application/json';
+    }
+    //If defaultAcceptType is not defined then default to application/json
+    if(config.defaultAcceptType === undefined) {
+        config.defaultAcceptType = 'application/json';
+    }
+
+    
+    // extract endpoint and path from url
+    var invokeUrl = 'https://w4jwq4ynp4.execute-api.us-east-1.amazonaws.com/stage';
+    var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
+    var pathComponent = invokeUrl.substring(endpoint.length);
+
+    var sigV4ClientConfig = {
+        accessKey: config.accessKey,
+        secretKey: config.secretKey,
+        sessionToken: config.sessionToken,
+        serviceName: 'execute-api',
+        region: config.region,
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType
+    };
+
+    var authType = 'NONE';
+    if (sigV4ClientConfig.accessKey !== undefined && sigV4ClientConfig.accessKey !== '' && sigV4ClientConfig.secretKey !== undefined && sigV4ClientConfig.secretKey !== '') {
+        authType = 'AWS_IAM';
+    }
+
+    var simpleHttpClientConfig = {
+        endpoint: endpoint,
+        defaultContentType: config.defaultContentType,
+        defaultAcceptType: config.defaultAcceptType
+    };
+
+    var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
+    
+    
+    
+    apigClient.fetchActionGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
+        
+        var fetchActionGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/fetch_action').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(fetchActionGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.fetchGameRecordGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['UserID'], ['body']);
+        
+        var fetchGameRecordGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/fetch_game_record').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['UserID']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(fetchGameRecordGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.fetchProfileGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['UserID'], ['body']);
+        
+        var fetchProfileGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/fetch_profile').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['UserID']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(fetchProfileGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.fetchProfileOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var fetchProfileOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/fetch_profile').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(fetchProfileOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.leaderboardGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var leaderboardGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/leaderboard').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(leaderboardGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.leaderboardOptions = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var leaderboardOptionsRequest = {
+            verb: 'options'.toUpperCase(),
+            path: pathComponent + uritemplate('/leaderboard').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(leaderboardOptionsRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.lobbyGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
+        
+        var lobbyGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/lobby').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(lobbyGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.lobbyRoomIdPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['room_id', 'body'], ['body']);
+        
+        var lobbyRoomIdPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/lobby/{room_id}').expand(apiGateway.core.utils.parseParametersToObject(params, ['room_id', ])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(lobbyRoomIdPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.postActionPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
+        
+        var postActionPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/post_action').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(postActionPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.profileGet = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['userId'], ['body']);
+        
+        var profileGetRequest = {
+            verb: 'get'.toUpperCase(),
+            path: pathComponent + uritemplate('/profile').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, ['userId']),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(profileGetRequest, authType, additionalParams, config.apiKey);
+    };
+    
+    
+    apigClient.profileEditPost = function (params, body, additionalParams) {
+        if(additionalParams === undefined) { additionalParams = {}; }
+        
+        apiGateway.core.utils.assertParametersDefined(params, ['body'], ['body']);
+        
+        var profileEditPostRequest = {
+            verb: 'post'.toUpperCase(),
+            path: pathComponent + uritemplate('/profile/edit').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
+            headers: apiGateway.core.utils.parseParametersToObject(params, []),
+            queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
+            body: body
+        };
+        
+        
+        return apiGatewayClient.makeRequest(profileEditPostRequest, authType, additionalParams, config.apiKey);
+    };
+    
+
+    return apigClient;
+};
