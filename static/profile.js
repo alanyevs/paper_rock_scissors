fb9f185bc1cb5a0bedfb9513594dd64c691adc56
Yanghui Pang <yanghuipang288@gmail.com>
2022-04-26 21:21:36 -0400

    profile api

--- static/profile.js
+++ static/profile.js
@@ -1,25 +1,37 @@
-let data = {
+let temp = {
     "avatar": "https://m.media-amazon.com/images/I/816p9XkbMQL._SL1427_.jpg",
     "user_id": "alanyevs",
     "winning_rate": "20%",
     "games": "200",
     "level":"17",
-    "paper_rate": "20%",
-    "scissors_rate": "20%",
-    "rock_rate": "20%",
+    "paper_rate": "30%",
+    "scissors_rate": "30%",
+    "rock_rate": "40%",
     "recent_games": []
 }
 
+function GetProfile(UserID) {
+    return sdk.fetchProfileGet({"UserID": UserID});
+}
+
 $(document).ready(function(){
-    $("#user_id").html(data.user_id)
-    $("#user_avatar").attr("src",data.avatar)
-    $("#total_wr").html(data.winning_rate)
-    $("#games").html(data.games)
-    $("#level").html(data.level)
-    $("#p_wr").html(data.paper_rate)
-    $("#r_wr").html(data.rock_rate)
-    $("#s_wr").html(data.scissors_rate)
-    $.each(data.recent_games, function(i, d){
+    GetProfile("d10106c1-66b2-478c-9742-2e346be2496f")
+    .then((response) => {
+        console.log(response);
+        let data = response.data;
+        $("#user_id").html(data.UserName)
+        $("#user_avatar").attr("src", temp.avatar) // TODO: 头像
+        $("#win_count").html(data.WinCount)
+        $("#game_count").html(data.GameCount)
+        $("#level").html(temp.level)  // ???? TODO: 等级
+        $("#p_wr").html(temp.paper_rate)  // TODO: 算概率
+        $("#r_wr").html(temp.rock_rate)
+        $("#s_wr").html(temp.scissors_rate)
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+    $.each(temp.recent_games, function(i, d){
         let r = $("<div class='row'>")
         let c = $("<div class='col-1'>")
         c.html(d.roomid)
